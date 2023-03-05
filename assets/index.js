585a1af3996645cb4a582980fbfb2d53a666ac82
Saul Tellez <saul.t.n.m@gmail.com>
2023-03-05 15:48:54 -0700

    First one

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+export { default as HeroImage } from "./hero.png";
+export { default as Avatar } from "./avatar.png";
+export { default as Hotels } from "./hotel.png";
+export { default as Attractions } from "./attraction.png";
+export { default as Restaurants } from "./restaurants.png";
+export { default as NotFound } from "./NotFound.png";
+export { default as Miku } from "./miku.png";
+export {default as KomiPanic} from "./KomiPanic.png";
+export { default as Bocchi} from "./Bocchi.jpg";
